9639be0735e39599caa750108f347205c0c2ec21
Josh Delany-Booth <joshdelany-booth@Joshs-MBP.lan>
2023-02-01 18:36:40 +0000

    changed mongo url

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,25 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
-const port = process.env.port || 27017
+const app = express();
+const port = process.env.port || 27017;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true })
-const connection = mongoose.connection
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully")
-})
+mongoose.connect("mongodb://127.0.0.1:27017/wedding_rsvps");
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
-const weddingRouter = require('./routes/weddingRsvp.js')
+const weddingRouter = require("./routes/weddingRsvp.js");
 
-app.use('/weddingRsvp', weddingRouter)
+app.use("/weddingRsvp", weddingRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`)
-})
+  console.log(`Server is running on port: ${port}`);
+});
