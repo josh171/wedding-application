a9dc1e50f7bf80eac7e2fcb5f84bedf3423d143b
Josh Delany-Booth <joshdelany-booth@Joshs-MBP.lan>
2023-02-01 18:15:25 +0000

    changed mongo atlas uri

--- backend/routes/weddingRsvp.js
+++ backend/routes/weddingRsvp.js
@@ -1,19 +1,19 @@
-const router = require('express').Router()
-let WeddingRSVP = require('../models/weddingRsvp.model')
+const router = require("express").Router();
+let WeddingRSVP = require("../models/weddingRsvp.model");
 
-router.route('/').get((req, res) => {
+router.route("/").get((req, res) => {
   WeddingRSVP.find()
-    .then(rsvp => res.json(rsvp))
-    .catch(err => res.status(400).json('Error: ' + err))
-})
+    .then((rsvp) => res.json(rsvp))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
-router.route('/add').post((req, res) => {
-  const name = req.body.name
-  const response = req.body.response
-  const starter = req.body.starter
-  const main = req.body.main
-  const dessert = req.body.dessert
-  const comments = req.body.comments
+router.route("/add").post((req, res) => {
+  const name = req.body.name;
+  const response = req.body.response;
+  const starter = req.body.starter;
+  const main = req.body.main;
+  const dessert = req.body.dessert;
+  const comments = req.body.comments;
 
   const newRSVP = new WeddingRSVP({
     name,
@@ -21,18 +21,19 @@ router.route('/add').post((req, res) => {
     starter,
     main,
     dessert,
-    comments
-  })
+    comments,
+  });
 
-  newRSVP.save()
-    .then(() => res.json('RSVP Added!'))
-    .catch(err => res.status(400).json('Error: ' + err))
-})
+  newRSVP
+    .save()
+    .then(() => res.json("RSVP Added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
-router.route('/:id').get((req, res) => {
+router.route("/:id").get((req, res) => {
   WeddingRSVP.findById(req.params.id)
-    .then(rsvp => res.json(rsvp))
-    .catch(err => res.status(400).json('Error: ' + err))
-})
+    .then((rsvp) => res.json(rsvp))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
-module.exports = router
+module.exports = router;
