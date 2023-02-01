ad14739aa9669a6b182e548d8f14dcb1d29973f5
Josh Delany-Booth <joshdelany-booth@Joshs-MBP.lan>
2023-02-01 21:10:52 +0000

    changed mongoose.connec

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 console.log(uri);
-mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.connect("mongodb://127.0.0.1:27017");
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
